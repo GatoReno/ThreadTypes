7c26c0464039d7938afdb92b30b1f4004c584bc4
Gatoreno <erp.reno@hotmail.com>
2020-11-23 17:18:42 -0500

    add monitor

--- Lock/Program.cs
+++ Lock/Program.cs
@@ -8,7 +8,7 @@ namespace Lock
         private static object _locket = new object();
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hellol Lock!");
+            Console.WriteLine("Hellol Monitor!");
             for (int i = 0; i < 7; i++)
             {
                 new Thread(DoWork).Start();
@@ -18,12 +18,24 @@ namespace Lock
 
         public static void DoWork()
         {
-            lock (_locket)
+            //lock (_locket)
+            Monitor.Enter(_locket);
+
+            try
             {
                 Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} starting ...");
                 Thread.Sleep(1000);
                 Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} completed ...");
             }
+            catch (Exception ex)
+            {
+                // error logger
+            }
+            finally
+            {
+                Monitor.Exit(_locket);
+            }
+          
         }
     }
 }
