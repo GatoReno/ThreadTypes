990f3c03dc57f3aa36d7892f284a7a76bc79b42a
Gatoreno <erp.reno@hotmail.com>
2020-11-23 15:42:09 -0500

    add lock

---
+++ Lock/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace Lock
+{
+    class MainClass
+    {
+        private static object _locket = new object();
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hellol Lock!");
+            for (int i = 0; i < 7; i++)
+            {
+                new Thread(DoWork).Start();
+            }
+            Console.ReadLine();
+        }
+
+        public static void DoWork()
+        {
+            lock (_locket)
+            {
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} starting ...");
+                Thread.Sleep(1000);
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} completed ...");
+            }
+        }
+    }
+}
---
+++ ThreadTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThreadTypes
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
