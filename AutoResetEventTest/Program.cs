ce7a9da65b2dedcd6114b4b40ac09636fde3cc7f
Gatoreno <erp.reno@hotmail.com>
2020-11-23 19:31:53 -0500

    adding auto reset event

---
+++ AutoResetEventTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace _autoresetEventTest
+{
+    class MainClass
+    {
+        static AutoResetEvent _autoreset = new AutoResetEvent(true);
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello ManualResetEvent!!!");
+
+           // new Thread(Write).Start();
+            for (int i = 0; i < 6; i++)
+            {
+                new Thread(Write).Start();
+            }
+
+            Console.ReadLine();
+        }
+
+
+        public static void Write()
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} w8ing ... ");
+            _autoreset.WaitOne();
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} w8ing ... ");
+             Thread.Sleep(5500);
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}  writing complete ... ");
+            _autoreset.Set();
+        }
+
+
+        public static void Read()
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} waiting ... ");
+             Thread.Sleep(2500);
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} read complete ... ");
+        }
+    }
+
+
+}
